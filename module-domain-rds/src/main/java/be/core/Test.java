69e417acb598474ebf7d9b635f4029db59e09d2d
kimJaeHyeon <kmr2644@gmail.com>
2023-01-19 02:49:19 +0900

    초기 모듈, 도커 세팅

---
+++ module-domain-rds/src/main/java/be/core/Test.java
@@ -0,0 +1,5 @@
+package be.core;
+
+public class Test {
+
+}
---
+++ module-domain-redis/src/test/java/be/moudledomainredis/MoudleDomainRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package be.moudledomainredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoudleDomainRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
